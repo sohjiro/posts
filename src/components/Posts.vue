5194d4ccab9498e3a6d3c10802f1a76234404db5
Sohjiro <felipe@makingdevs.com>
2020-03-30 02:30:14 -0600

    adding like counter

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
-    <div class="media">
-      <img src="https://via.placeholder.com/64" class="align-self-start mr-3" alt="...">
-      <div class="media-body">
-        <h5 class="mt-0">{{ post.title }}</h5>
-        {{ post.body }}
+    <div class="card w-75">
+      <div class="card-body">
+        <div class="card-title">
+          <img src="https://via.placeholder.com/64" class="rounded-circle" alt="...">
+          Usuario
+        </div>
+
+        <p class="card-text"> {{ post.body }} </p>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card-text">
+              <small class="text-muted">3 years ago</small>
+            </div>
+          </div>
+          <div class="col-md-8 text-right">
+            <button type="button" class="btn btn-light" @click="counter += 1"> {{ counter }} ♡</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -13,6 +26,11 @@
 <script>
 
 export default {
+  data: function() {
+    return {
+      counter: 0
+    }
+  },
   props: {
     post: {
       id: Number,
