b983fc4859e53f4cec7540347e5f3583df62ebec
Sohjiro <felipe@makingdevs.com>
2020-03-30 02:09:19 -0600

    showing posts

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Posts />
+    <Posts v-for="post in posts" :key="post.id" :post="post" />
   </div>
 </template>
 
 <script>
 import Posts from './components/Posts.vue'
+import axios from "axios"
 
 export default {
   name: 'App',
   components: {
     Posts
+  },
+  data: () => {
+    return {
+      posts: []
+    }
+  },
+  mounted: function() {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then((result) => {
+        console.log(result.data)
+        this.posts = result.data
+      })
   }
 }
 </script>
@@ -21,7 +33,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
-    <h1>Hello world</h1>
+    <div class="media">
+      <img src="https://via.placeholder.com/64" class="align-self-start mr-3" alt="...">
+      <div class="media-body">
+        <h5 class="mt-0">{{ post.title }}</h5>
+        {{ post.body }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
+  props: {
+    post: {
+      id: Number,
+      title: String,
+      body: String,
+      userId: Number
+    }
+  }
 }
 </script>
 
-<style></style>
+<style scoped>
+</style>
 
